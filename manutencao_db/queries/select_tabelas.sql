0b20a451a80453a957db1c69988ec320734b062f
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2026-01-29 10:30:28 -0300

    refactor: reestrutura projeto

--- manutencao_db/queries/select_tabelas.sql
+++
@@ -1,8 +0,0 @@
--- Tabela Manutenção
-select * from manutencao;
--- Tabela de Sistemas
-select * from tipo_sistema;
--- Tabela de Operações
-select * from tipo_operacao;
--- Tabela de Criticidades
-select * from tipo_criticidade;
