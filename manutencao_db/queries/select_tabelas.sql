c759305f2df9e87284446a4f9387e7fa235babd7
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2026-01-29 09:53:30 -0300

    feat: implementa estrutura de script de select de tabelas

---
+++ manutencao_db/queries/select_tabelas.sql
@@ -0,0 +1,8 @@
+-- Tabela Manutenção
+select * from manutencao;
+-- Tabela de Sistemas
+select * from tipo_sistema;
+-- Tabela de Operações
+select * from tipo_operacao;
+-- Tabela de Criticidades
+select * from tipo_criticidade;
