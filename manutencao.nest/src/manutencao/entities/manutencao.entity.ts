d9d012f127874c45e0780f63c9bba53d7124f294
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2026-01-30 11:18:02 -0300

    feat: entidade manutencao mapeada e registrada

--- manutencao.nest/src/manutencao/entities/manutencao.entity.ts
+++ manutencao.nest/src/manutencao/entities/manutencao.entity.ts
@@ -1,1 +1,38 @@
-export class Manutencao {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('manutencao') //Nome de Tabela
+export class Manutencao {
+  @PrimaryGeneratedColumn('identity', { name: 'codigo' })
+  codigo: number;
+
+  @Column({ name: 'codigo_tipo_sistema' })
+  codigoTipoSistema: number;
+
+  @Column({ name: 'codigo_tipo_operacao' })
+  codigoTipoOperacao: number;
+
+  @Column({ name: 'codigo_tipo_criticidade' })
+  codigoTipoCriticidade: number;
+
+  @CreateDateColumn({
+    name: 'data_agendamento',
+    type: 'timestamp',
+    nullable: true,
+  })
+  dataAgendamento: Date;
+
+  @CreateDateColumn({
+    name: 'data_finalizada',
+    type: 'timestamp',
+    nullable: true,
+  })
+  dataFinalizada: Date;
+
+  @Column({ type: 'text', nullable: true }) //Texto Longo "text" e Opcional "nullable: true"
+  descricao: string;
+}
--- manutencao.nest/src/manutencao/manutencao.module.ts
+++ manutencao.nest/src/manutencao/manutencao.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // Importação
 import { ManutencaoService } from './manutencao.service';
 import { ManutencaoController } from './manutencao.controller';
+import { Manutencao } from './entities/manutencao.entity'; // Importação
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Manutencao])], // Importação
   controllers: [ManutencaoController],
   providers: [ManutencaoService],
 })
