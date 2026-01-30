dfc40f077715a80c33223cff0d7fffc762f07d49
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2026-01-30 10:55:17 -0300

    feat: configuracao de conexao com o banco de dados

--- manutencao.nest/src/app.module.ts
+++ manutencao.nest/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgre123',
+      database: 'manutencao_db',
+      entities: [],
+      synchronize: false, //Tabelas já Criadas no Banco Local
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
